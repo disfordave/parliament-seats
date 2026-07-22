2a632dc458aac8ff990eeae39dddedcfb489905c
Dave <118544840+disfordave@users.noreply.github.com>
2026-07-22 16:30:42 +0900

    fix: simplify footer component styling; remove unnecessary margin class

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,9 +25,7 @@ export default function Footer() {
   const { i } = useI18n();
   return (
     <>
-      <footer
-        className={"-mb-4 mt-4 rounded-t-2xl bg-zinc-200 p-4 dark:bg-zinc-700"}
-      >
+      <footer className={"mt-4 rounded-2xl bg-zinc-200 p-4 dark:bg-zinc-700"}>
         <div
           className={
             "flex w-full flex-col items-start justify-between gap-4 sm:flex-row sm:items-center"
